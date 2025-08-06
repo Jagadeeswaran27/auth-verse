9d1cae1eb9f373ba18b87fa0b717c3d2ee22048f
Jagadeeswaran27 <jagadeeswaran2705@gmail.com>
2025-08-06 21:22:23 +0530

    initial files for auth-verse

---
+++ frontend/src/app.tsx
@@ -0,0 +1,11 @@
+export function App() {
+  return (
+    <div>
+      <header className="font-bold text-customColor">
+        <h1 className="text-red-500">Auth Verse</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
